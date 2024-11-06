87a3422c1214a6b507bb777a28b27e052c08e9e4
Alex <alex@playbook3d.com>
2024-11-06 10:26:59 -0600

    Added Playbook Text

---
+++ playbookText.py
@@ -0,0 +1,29 @@
+class PlaybookText:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return{
+            "required": {
+                "id": ("STRING", { "multiline": False, "default": "Playbook Text ID"},),
+            },
+            "optional": {
+                "text": ("STRING", {"multiline": True},)
+            }
+        }
+    
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("Text",)
+
+    FUNCTION = "parse_text"
+
+    OUTPUT_NODE = { False }
+
+    CATEGORY = "Playbook3 d"
+
+    def parse_text(self, text = None):
+        return [text]
+    
+NODE_CLASS_MAPPINGS = { "Playbook Text": PlaybookText}
+NODE_DISPLAY_NAME_MAPPINGS = { "Playbook Text": "Playbook Text"}
