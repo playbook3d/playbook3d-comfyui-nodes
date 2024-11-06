4c0f23744251bbdc0fbbc9bb03419d8ffb724c0e
Alex <alex@playbook3d.com>
2024-11-06 11:30:13 -0600

    Updated Playbook Number, Added Playbook Float

--- playbookFloat.py
+++ playbookFloat.py
@@ -1,4 +1,4 @@
-class PlaybookNumber:
+class PlaybookFloat:
     def __init__(self):
         pass
 
@@ -6,36 +6,37 @@ class PlaybookNumber:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "id": ("STRING", {"multiline": False, "default": "Playbook Number ID"} )
+                "id": ("STRING", {"multiline": False, "default": "Playbook Float ID"})
             },
             "optional": {
-                "playbook_number": ("INT",
+                "playbook_float": ("FLOAT",
                     {
                         "multiline": True, 
                         "display": "number", 
                         "min": -2147483647, 
                         "max": 2147483647, 
-                        "default": 0
+                        "default": 0,
+                        "step": 0.01
                     },
                 ),
             }
         }
 
-    RETURN_TYPES = { "INT", }
-    RETURN_NAMES = { "Int", }
+    RETURN_TYPES = { "FLOAT", }
+    RETURN_NAMES = { "Float", }
 
-    FUNCTION = "parse_number"
+    FUNCTION = "parse_float"
 
     OUTPUT_NODE = {}
 
     CATEGORY = { "Playbook 3D" }
 
-    def parse_number(self, id,  playbook_number = None):
+    def parse_float(self, id,  playbook_number = None):
         if not id or (isinstance(id, str) and not id.strip().isdigit()):
             return [playbook_number]
-        return [int(id)]
+        return [float(id)]
 
 
-NODE_CLASS_MAPPINGS = { "Playbook Number": PlaybookNumber }
+NODE_CLASS_MAPPINGS = { "Playbook Float": PlaybookFloat }
 
-NODE_DISPLAY_NAME_MAPPINGS = { "Playbook Number": "Playbook Number (External)" }
+NODE_DISPLAY_NAME_MAPPINGS = { "Playbook Float": "Playbook Float (External)" }
