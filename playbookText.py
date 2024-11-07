036089497894aef379b5e1c16e55d41e1995ec6f
Alex <alex@playbook3d.com>
2024-11-07 10:18:23 -0600

    Update PlaybookText

--- playbookText.py
+++ playbookText.py
@@ -9,12 +9,13 @@ class PlaybookText:
                 "id": ("STRING", { "multiline": False, "default": "Playbook Text ID"},),
             },
             "optional": {
-                "text": ("STRING", {"multiline": True},)
+                "text": ("STRING", {"multiline": True},),
+                "trigger_words": ("STRING", {"multiline": True})
             }
         }
     
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("Text",)
+    RETURN_TYPES = ("STRING", "STRING")
+    RETURN_NAMES = ("Text", "Trigger Words")
 
     FUNCTION = "parse_text"
 
@@ -22,8 +23,8 @@ class PlaybookText:
 
     CATEGORY = "Playbook3 d"
 
-    def parse_text(self, text = None):
-        return [text]
+    def parse_text(self, text = None, trigger_words = None):
+        return [text, trigger_words]
     
 NODE_CLASS_MAPPINGS = { "Playbook Text": PlaybookText}
 NODE_DISPLAY_NAME_MAPPINGS = { "Playbook Text": "Playbook Text (External)"}
