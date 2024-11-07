1db76509e9ffcc1be6e1db1bbf58c8b4f2780c92
Alex <alex@playbook3d.com>
2024-11-07 09:56:21 -0600

    Added PlaybookBoolean

---
+++ playbookBoolean.py
@@ -0,0 +1,29 @@
+class PlaybookBoolean:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "id": ("STRING", {"multiline": False, "default": "Playbook Boolean ID"}),
+                "playbook_bool": ("BOOLEAN",{"default": False})
+            },
+        }
+
+    RETURN_TYPES = { "BOOLEAN", }
+    RETURN_NAMES = { "Boolean", }
+
+    FUNCTION = "parse_boolean"
+
+    OUTPUT_NODE = {}
+
+    CATEGORY = { "Playbook 3D" }
+
+    def parse_boolean(self, id,  playbook_bool = None):
+        return [playbook_bool]
+
+
+NODE_CLASS_MAPPINGS = { "Playbook Boolean": PlaybookBoolean }
+
+NODE_DISPLAY_NAME_MAPPINGS = { "Playbook Boolean": "Playbook Boolean (External)" }
