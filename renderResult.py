e44641484ee6d57068bc0b2ed59e23cb5e6d4ae6
Alex Carrizosa <alex.carrizosa@outlook.com>
2024-11-13 04:46:27 -0600

    Fixed render pass result

--- renderResult.py
+++ renderResult.py
@@ -1,6 +1,8 @@
 import requests
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image
+from io import BytesIO
+from base64 import b64encode
 
 class UploadRenderResult:
     def __init__(self):
@@ -10,7 +12,7 @@ class UploadRenderResult:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "image": ("IMAGE",),
+                "images": ("IMAGE",),
                 "api_key": ("STRING", { "multiline": False })
             },
         }
@@ -24,16 +26,22 @@ class UploadRenderResult:
 
     CATEGORY = "Playbook 3D"
 
-    def parse_result(self, api_key, image):
+    def parse_result(self, api_key, images):
+        i = 255. * images.cpu().numpy()
+        i = np.squeeze(i)
+        img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8), 'RGB')
+        buffer = BytesIO()
+        img.save(buffer, "PNG")
+        buffer.seek(0)
+        img_data = buffer.getvalue()
 
-        internal_img = 255 * image.cpu().numpy()
-        img = Image.fromarray(np.clip(internal_img, 0, 255).astype(np.uint8))
         base_url = "https://accounts.playbookengine.com"
         user_token = None
         jwt_request = requests.get(f"{base_url}/token-wrapper/get-tokens/{api_key}")
 
         try:
             if jwt_request is not None:
+
                 user_token = jwt_request.json()["access_token"]
         except Exception as e:
             print(f"Error with node: {e}")
@@ -43,17 +51,15 @@ class UploadRenderResult:
             headers = {"Authorization": f"Bearer {user_token}"}
             result_request = requests.get(f"{base_url}/upload-assets/get-upload-urls", headers=headers)
             if result_request.status_code == 200:
-                print(result_request.json())
                 result_url = result_request.json()["save_result"]
-                result_response = requests.put(url=result_url, files=img)
-                print(result_response)
+                result_response = requests.put(url=result_url, data=img_data)
                 if result_response.status_code == 200:
-                    download_request = requests.get(f"{base_url}/download-assets/get-download-urls", headers=headers)
-                    print(download_request.json())
+                    download_request = requests.get(f"{base_url}/upload-assets/get-download-urls", headers=headers)
                     download_url = download_request.json()["save_result"]
                     return [download_url]
         except Exception:
             raise ValueError("Error with uploading Result")
+        raise ValueError("Error with uploading Result")
 
 
 NODE_CLASS_MAPPINGS = {
