efed9ed4762e5a74cbfee760ae5c5a1651cc9398
Priyansh Bhatter <pbhatter@usc.edu>
2024-12-31 13:05:05 +0530

    converted video into images as output

--- playbookVideo.py
+++ playbookVideo.py
@@ -1,9 +1,7 @@
-# playbookVideo.py
 import torch
 import numpy as np
 import cv2
 import requests
-from io import BytesIO
 import tempfile
 import os
 
@@ -26,21 +24,17 @@ class PlaybookVideo:
             },
         }
 
-    RETURN_TYPES = ("VIDEO",)
-    RETURN_NAMES = ("Video",)
-
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("Frames",)
     FUNCTION = "parse_video"
-
     OUTPUT_NODE = False
-
     CATEGORY = "Playbook 3D"
 
     def parse_video(self, id, label, frame_load_cap=0, skip_first_frames=0, select_every_nth=1, default_value=None):
         video_url = id
-        video = default_value
         frames = []
         try:
-            if id.startswith('http'):
+            if video_url.startswith('http'):
                 video_request = requests.get(video_url, stream=True)
                 if video_request.status_code == 200:
                     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_video_file:
@@ -48,51 +42,39 @@ class PlaybookVideo:
                             if chunk:
                                 temp_video_file.write(chunk)
                         temp_video_file_path = temp_video_file.name
-
                     cap = cv2.VideoCapture(temp_video_file_path)
                 else:
                     raise ValueError("Failed to download video")
             else:
                 raise ValueError("Invalid URL")
-
             frame_counter = 0
             frames_loaded = 0
-
             while cap.isOpened():
                 ret, frame = cap.read()
                 if not ret:
                     break
-
                 frame_counter += 1
-
                 if frame_counter <= skip_first_frames:
                     continue
-
                 if (frame_counter - skip_first_frames - 1) % select_every_nth != 0:
                     continue
-
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 frame = frame.astype(np.float32) / 255.0
                 frame_tensor = torch.from_numpy(frame).permute(2, 0, 1)
-
                 frames.append(frame_tensor)
-
                 frames_loaded += 1
                 if frame_load_cap > 0 and frames_loaded >= frame_load_cap:
                     break
-
             cap.release()
-
             if os.path.exists(temp_video_file_path):
                 os.remove(temp_video_file_path)
-
             if frames:
-                video = torch.stack(frames)
-
-            return [video]
+                return [torch.stack(frames, dim=0)]
+            else:
+                return [None]
         except Exception as e:
-            print(f"Exception while downloading or processing video: {e}")
-            return [video]
+            print(f"Exception while downloading or processing frames: {e}")
+            return [None]
 
 NODE_CLASS_MAPPINGS = {
     "Playbook Video": PlaybookVideo
